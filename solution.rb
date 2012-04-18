06c3878bafa88819ecb5f81e741286244a89378d
Matthias Fritsche <matthias.fritsche@rwth-aachen.de>
2012-04-18 16:49:58 +0200

    method to find shortest distance by brute force added
    fixed issues with the Math class
    added a result class that stores all the information that are relevant
    for the result

--- solution.rb
+++ solution.rb
@@ -1,17 +1,20 @@
 #!/usr/bin/ruby 
-require 'math'
 class Point
 	attr_accessor :x,:y
 	def initialize(x,y)
 		@x = x
 		@y = y
 	end
+	
+	def to(other_point)
+		distance(self, other_point)
+	end
 end
 
-def generate_points
+def generate_points(amount = 1000)
 	points = Array.new
 	prng = Random.new(1234)
-	1000.times do 
+	amount.times do 
 		points << Point.new(prng.rand(0.0..1.0), prng.rand(0.0..1.0))
 	end
 	points
@@ -46,12 +49,45 @@ def find_shortest_distance(points)
 
 end
 
+#class used to store the result
+class Distance
+	attr_accessor :distance, :point1, :point2
+	def initialize( point1, point2, distance = nil)
+		set(point1, point2, distance)
+	end
+	def set(point1, point2, distance = nil)
+		if distance.nil?
+			@distance = point1.to(point2)
+		else
+			@distance = distance
+		end
+		@point1 = point1
+		@point2 = point2
+	end
+end
+
+def find_shortest_distance_of_array(points)
+	# make sure we don't have too many points to compare
+	if points.count > 9
+		raise "Too many points in Array!"
+	end
+
+	shortest_distance = Distance.new(Point.new(0,0), Point.new(1,1))
+	points.combination(2).each do |point1, point2|
+		distance = point1.to point2
+		if distance < shortest_distance.distance
+			shortest_distance.set(point1, point2, distance)
+		end
+	end
+	shortest_distance
+end
+
 def create_areas(points)
 	# so we need to find a grid pattern that makes sure that an Area and all its neighbors
 	# don't include more than 6 points (maybe 5 is a better upper bound)
 	# trial and error
 	areas = Array.new
-	area_length = Math.floor(Math.sqrt(points.length))
+	area_length = Math.sqrt(points.length).floor
 
 	#build area including the left side, excluding the right side
 	left = 0
