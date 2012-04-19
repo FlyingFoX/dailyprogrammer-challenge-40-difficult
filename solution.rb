02e3280de4e341703f8897930b8908fdc61ac824
Matthias Fritsche <matthias.fritsche@rwth-aachen.de>
2012-04-19 12:29:47 +0200

    to_s added; refactoring and fixes; added result output
    to_s method added to Point and Distance
    renamed points_near
    added output to the main method.

--- solution.rb
+++ solution.rb
@@ -9,6 +9,10 @@ class Point
 	def to(other_point)
 		distance(self, other_point)
 	end
+
+	def to_s
+		"Point (#{@x}, #{@y})"
+	end
 end
 
 def generate_points(amount = 1000)
@@ -27,28 +31,49 @@ end
 class Area
 	attr_accessor :neighbors, :points, :top_left, :bottom_right
 	def initialize(top_left, bottom_right)
-		unless top_left.kind_of Point and bottom_right.kind_of Point
+		unless top_left.kind_of? Point and bottom_right.kind_of? Point
 			raise "wrong input"
 		end
 		@top_left = top_left
 		@bottom_right = bottom_right
+		@neighbors = Array.new
+		@points = Array.new
 	end
+	end
+
 	def in_area?(point)
 		point.x >= top_left.x and point.x <= bottom_right and 
-		point.y >= bottom_right and point.y <= top_left
+				point.y >= bottom_right and point.y <= top_left
 	end
-	def points_near
+
+	def number_of_points_near
 		points.length + neighbors.inject{ |sum, neighbor| sum += neighbor.points.length }
+	end
+	
+	def points_near
+		points + neighbors.inject{|other_points, neighbor| other_points += neighbor.points }
 	end
 end
 
 def find_shortest_distance(points)
 	# first sort points into areas 
 	# then you have to only compare all points in neighbouring areas for their distance
-
-
+	areas = create_areas(points)
+	shortest_distance = Distance.new(Point.new(0,0), Point.new(1,1))
+	areas.each do |area|
+		current_distance = find_shortest_distance_in_area(area)
+		if shortest_distance.distance > current_distance.distance
+			shortest_distance = current_distance
+		end
+	end
+	shortest_distance
 end
 
+def find_shortest_distance_in_area(area)
+	find_shortest_distance_of_array(area.points_near)
+end
+		
+	
 #class used to store the result
 class Distance
 	attr_accessor :distance, :point1, :point2
@@ -63,6 +88,9 @@ class Distance
 		end
 		@point1 = point1
 		@point2 = point2
+	end
+	def to_s
+		"distance between #{point1} and #{point2} is #{@distance}"
 	end
 end
 
@@ -101,10 +129,17 @@ def create_areas(points)
 		end
 		left = right
 	end
+
+	# add the points to the areas!
+	# i am under the impression that i need some kind of area tree to be able to do this
+	areas.each do|area|
+		if area.in_area?
+		end
+	end
 	areas
 end
 
 if __FILE__ == $0
 	points = generate_points
-	find_shortest_distance(points)
+	puts "The shortest found distance is:\n#{find_shortest_distance(points)}"
 end
